02389ff415f9fd0f60c5ddc7f4e6cd6c2f76f598
hongpro99 <164489043+hongpro99@users.noreply.github.com>
2024-09-29 17:36:32 +0900

    Update app.py

--- langchain/app.py
+++ langchain/app.py
@@ -189,4 +189,4 @@ def save_to_db(question, keywords, recommendations):
 '''
 # Flask 서버 실행
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
